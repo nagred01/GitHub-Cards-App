119925cf6ffdeef42f27d6f7152592cb4639a789
Nagender reddy <nagenderreddy4779@gmail.com>
2020-05-04 14:53:29 -0700

    Using Axios to get the real data from api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
+import { identifier } from "@babel/types";
+
+const CardList = props => (
+  <div>
+    {props.profiles.map(profile => (
+      <Card key={profile.id} {...profile} />
+    ))}
+  </div>
+);
+
+class Form extends React.Component {
+  state = { userName: "" };
+  handleSubmit = async e => {
+    e.preventDefault();
+    const resp = await axios.get(
+      `https:api.github.com/users/${this.state.userName}`
+    );
+    this.props.onSubmit(resp.data);
+    this.setState({ userName: "" });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={e => this.setState({ userName: e.target.value })}
+          placeholder="Github username"
+          required
+        />
+        <button>Add card</button>
+      </form>
+    );
+  }
+}
 
 class Card extends React.Component {
   render() {
+    const profile = this.props;
     return (
-      <div className="github-profile">
-        <img src="https://placehold.it/75" />
-        <div className="info">
-          <div className="name">Name here...</div>
-          <div className="company">Company here...</div>
+      <div className="github-profile" style={{ margin: "1rem" }}>
+        <img src={profile.avatar_url} />
+        <div
+          className="info"
+          style={{ display: "inline-block", marginLeft: 10 }}
+        >
+          <div className="name" style={{ fontSize: "125%" }}>
+            {profile.name}
+          </div>
+          <div className="company">{profile.company}</div>
         </div>
       </div>
     );
@@ -16,11 +58,21 @@ class Card extends React.Component {
 }
 
 class App extends React.Component {
+  state = {
+    profiles: []
+  };
+  addNewProfile = profileData => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData]
+    }));
+  };
+
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-        <Card />
+        <Form onSubmit={this.addNewProfile} />
+        <CardList profiles={this.state.profiles} />
       </div>
     );
   }
